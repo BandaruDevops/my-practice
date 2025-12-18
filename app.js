31839201b17c0a8f26d4cab00baf686f8827f2ac
BandaruDevops <bandaru.rajkumar19@gmail.com>
2025-12-18 10:32:46 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js App!');
+});
+
+app.listen(port, () => console.log(`Listening on ${port}`));
